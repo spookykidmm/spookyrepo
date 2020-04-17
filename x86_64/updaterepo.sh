00eb3500cc806fa75da646d0ca90d2098f15f5ad
spookykidmm <spookykidmm@gmail.com>
2020-04-17 15:40:24 -0400

    Fixed repo files

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm revenge_repo.*
-repo-add revenge_repo.db.tar.gz *.pkg.tar.xz
+rm spookyrepo.*
+repo-add spookyrepo.db.tar.gz *.pkg.tar.xz
 sleep 2
-rm revenge_repo.db revenge_repo.files
+rm spookyrepo.db spookyrepo.files
 sleep 2
-cp revenge_repo.db.tar.gz revenge_repo.db
+cp spookyrepo.db.tar.gz spookyrepo.db
 sleep 2
-cp revenge_repo.files.tar.gz revenge_repo.files
+cp spookyrepo.files.tar.gz spookyrepo.files
 echo "Repo Updated!!"
